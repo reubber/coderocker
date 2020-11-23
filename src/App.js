f5feb40a8be6636cc75960a69b65dc1b5e57b06b
reubber sousa <reubbers@alu.ufc.br>
2020-11-23 15:44:34 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-function App() {
+import React from 'react'
+import Routes from './routes'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Rubik', sans-serif;
+    
+  }
+
+  body {
+    background: rgb(53,36,2);
+    background: linear-gradient(270deg, rgba(53,36,2,1) 0%, rgba(241,161,10,1) 85%); 
+    background-repeat:none;
+  }
+
+  p {
+    font-weight: normal;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`
+
+function App () {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Header from './components/Header'
+import Post from './components/Post'
+
+function Routes () {
+  return (
+    <Router>
+        <Header />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/post/:id' exact component={Post} />
+      </Switch>
+    </Router>
+  )
+}
+
+export default Routes
