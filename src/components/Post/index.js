528fbae60e6183fc4f528d04ef990153f21dbbc2
reubber sousa <reubbers@alu.ufc.br>
2020-12-02 21:23:25 -0300

    add media-querys in post

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -7,16 +7,33 @@ const Container = styled.div`
   margin: 100px auto;
   background:white;
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.1);
+  @media (max-width: 80rem){
+    height: 1200px;
+  }
 `
 const Image = styled.img`
   width: 50%;
   height: 40%;
+  @media (max-width: 80rem){
+    width: 100%; 
+  }
+`
+const Wrapper = styled.div`
+  display: flex;
+  @media (max-width: 80rem){
+    flex-direction: column; 
+  }
 `
 
 const Content = styled.div`
   height: 595px;
   padding: 80px 0 0 60px;
   margin-top: 50px;
+  @media (max-width: 80rem){
+    height: 280px;
+    padding:0;
+    margin: 0; 
+  }
 `
 const Title = styled.h1`
   margin-bottom: 10px;
@@ -33,6 +50,10 @@ const Article = styled.div`
   padding: 10% 20%;
   color: #2D2D2D;
   width: 100%;
+  @media (max-width: 80rem){
+    padding:0;
+    margin: 0; 
+  }
 `
 
 function Post (props) {
@@ -41,14 +62,14 @@ function Post (props) {
   return (
     <>
       <Container>
-        <div style={{display:'flex'}}>
+        <Wrapper>
         <Image src={img} alt=""/>
         <Content>
           <Text>{name}</Text>
           <Title>{title}</Title>
         
         </Content> 
-        </div>
+        </Wrapper>
         <Article>{article}</Article>
       
       </Container>  
